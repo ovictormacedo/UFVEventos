a8537e51d3d32977bceecec9b02d64e3e01b6cc0
Victor <ovictormacedo@gmail.com>
2017-07-17 01:55:59 -0300

    Part of the interfaces

--- app/src/main/java/com/example/vma/ufveventos/login.java
+++ app/src/main/java/com/example/vma/ufveventos/login.java
@@ -2,6 +2,16 @@ package com.example.vma.ufveventos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
 
 public class login extends AppCompatActivity {
 
@@ -9,5 +19,21 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void entrar(View view){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://meettest.esy.es/API/api.php/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        Categoria service = retrofit.create(Categoria.class);
+
+        Call<List<String>> repos = service.listRepos();
+        try {
+            List<String> aux = repos.execute();
+        }catch (java.io.IOException e){Toast.makeText(this,"ERRO:"+e.getMessage(),Toast.LENGTH_SHORT).show();}
+
+        Toast.makeText(this,repos.toString(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this,"Clicou",Toast.LENGTH_SHORT).show();
     }
 }
