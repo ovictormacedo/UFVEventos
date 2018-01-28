986c443673f45093c7f304b6a07cf3f921ac0e12
Victor <ovictormacedo@gmail.com>
2018-01-28 02:57:50 -0200

    .

---
+++ app/src/main/java/com/example/vma/ufveventos/controller/sobre.java
@@ -0,0 +1,15 @@
+package com.example.vma.ufveventos.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vma.ufveventos.R;
+
+public class sobre extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sobre);
+    }
+}
